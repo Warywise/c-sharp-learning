469fd17bdd3f9b86312178c13c153c586f476077
Gustavo Sant'Anna <g_santanna@outlook.com>
2024-08-01 01:23:02 -0300

    chore: initializing variables

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,7 +3,8 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine(123);
 Console.WriteLine(0.25F);
 Console.WriteLine(2.625);
-Console.WriteLine(12.39816M);
+Console.WriteLine(12.398160M);
+Console.WriteLine(12.398160F);
 Console.WriteLine(true);
 Console.WriteLine(false);
 
@@ -12,3 +13,11 @@ Console.WriteLine(123);
 
 Console.WriteLine("true");
 Console.WriteLine(true);
+
+// inicializando com tipo explícito
+string firstName;
+firstName = "Bob";
+// inicializando com tipo implícito (o tipo é inferido, sempre deve ser inicializado)
+// var lastName; // erro
+var lastName = "Smith";
+Console.WriteLine(firstName + " " + lastName);
