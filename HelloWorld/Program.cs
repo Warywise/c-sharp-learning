2180059a778a1b80a651d087d6a69677152261bc
Gustavo Sant'Anna <g_santanna@outlook.com>
2024-08-01 13:04:15 -0300

    chore: basic logical operations

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -42,3 +42,53 @@ Directory:           {directory}
 Qnt:                 {numberOfDocs}";
 
 Console.WriteLine(directoryExplain);
+
+Console.WriteLine();
+
+// Misturar tipos de dados
+int age = 30;
+decimal height = 1.75M;
+string name = "Bob";
+Console.WriteLine($"{name} tem {age} anos e {height}m de altura.");
+// somar strings com números
+Console.WriteLine("O resultado de 2 + 3 não é " + 2 + 3 + $", e nem {age}, mas sim " + (2 + 3));
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+// divisão de inteiros resulta em inteiro
+int intQuotient = 7 / 5;
+// divisão de decimais resulta em decimal
+decimal decimalQuotient = 7M / 5;
+// conversão de decimal para inteiro, mesmo com tipo de dado decimal o valor é convertido para inteiro
+decimal convertedInt = (int)decimalQuotient;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+// resultará em 1, pois o tipo de dado é inteiro
+Console.WriteLine("Int Quotient: " + intQuotient);
+// resultará em 1.4, pois o tipo de dado é decimal
+Console.WriteLine("Decimal Quotient: " + decimalQuotient);
+Console.WriteLine("Converted Int: " + convertedInt);
+// operador de módulo
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+
+Console.WriteLine();
+int value = 0;     // value is now 0.
+value = value + 5; // value is now 5.
+value += 5;        // value is now 10.
+value -= 5;        // value is now 5.
+value++;           // value is now 6.
+value--;           // value is now 5.
+value *= 2;        // value is now 10.
+value /= 2;        // value is now 5.
+Console.WriteLine(value);
+// dependendo da posição do operador, o valor pode ser incrementado antes ou depois da operação
+// Ex: ++x faz o incremento antes de recuperar o valor, x++ faz o incremento depois de recuperar o valor
+int x = 5;
+int y = x++; // y = 5, x = 6
+Console.WriteLine($"x = {x}, y = {y}");
+x = 5;
+y = ++x; // y = 6, x = 6
+Console.WriteLine($"x = {x}, y = {y}");
--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a87f244550845065b17e7768e23fa90747138b76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da074c2bc9d220ada098bb68ad2bf12f0f88b415")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
