52319803c2e8dd1c04732cc0b91a57fe2521cb6e
Gustavo Sant'Anna <g_santanna@outlook.com>
2024-08-01 23:31:48 -0300

    chore: simple projects, calc Student Grade & GPA

--- student-grades-project/obj/Debug/net8.0/student-grades-project.AssemblyInfo.cs
+++ student-grades-project/obj/Debug/net8.0/student-grades-project.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("student-grades-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2180059a778a1b80a651d087d6a69677152261bc")]
-[assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
+[assembly: System.Reflection.AssemblyProductAttribute("student-grades-project")]
+[assembly: System.Reflection.AssemblyTitleAttribute("student-grades-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
