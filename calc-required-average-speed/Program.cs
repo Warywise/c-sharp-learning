3f5742b26642ee7b8d5ce63c7c393eca90239c44
Gustavo Sant'Anna <g_santanna@outlook.com>
2024-08-02 16:02:26 -0300

    refact: improves average speed project

--- calc-required-average-speed/Program.cs
+++ calc-required-average-speed/Program.cs
@@ -6,14 +6,33 @@ Console.WriteLine("Primeiramente, qual o seu nome?");
 string name = Console.ReadLine();
 Console.WriteLine("Olá, " + name + "! Vamos calcular a velocidade média necessária para percorrer uma distância em um determinado tempo. Para isso, precisaremos de alguns dados.");
 
-// TODO: Implementar lógica para usuário aceitar ou não continuar
+Console.WriteLine("Para continuar, digite: OK");
+string userResponse= Console.ReadLine();
+
+if (userResponse != null && userResponse.ToUpper() != "OK")
+{
+  Console.WriteLine("Encerrando o programa...");
+  return;
+}
 
 Console.WriteLine("Informe a distância a ser percorrida em quilômetros: ");
 decimal distance = decimal.Parse(Console.ReadLine());
 
 // TODO: Verificar possibilidade de aguardar um tempo em segundos até próxima execução
 
-Console.WriteLine("Informe o tempo disponível para percorrer essa distância em horas: ");
+Console.WriteLine("Escolha a unidade de tempo para o cálculo da velocidade média:");
+Console.WriteLine("Digite \" 1 \", para Minutos");
+Console.WriteLine("Digite \" 2 \", para Horas");
+int timeUnit = int.Parse(Console.ReadLine());
+
+if (timeUnit != 1 && timeUnit != 2)
+{
+  // TODO: implementar lógica para repetir o fluxo de escolha da unidade de tempo
+  Console.WriteLine("Opção inválida. Encerrando o programa...");
+  return;
+}
+
+Console.WriteLine("Informe o tempo disponível para percorrer essa distância: ");
 decimal time = decimal.Parse(Console.ReadLine());
 
 // TODO: Impementar lógica para perguntar se deseja utilizar minutos ou horas
@@ -23,4 +42,6 @@ decimal speed = distance / time;
 
 // SAÍDA DE DADOS
 Console.WriteLine("Aqui está sua estimativa " + name + ":");
-Console.WriteLine("Para percorrer " + distance.ToString("N2") + " quilômetros em " + time.ToString("N2") + " horas, " + "você precisará de uma velocidade média de " + speed.ToString("N2") + " km/h.");
+Console.WriteLine($"Para percorrer {distance} km em {time} {(timeUnit == 1 ? "minutos" : "horas")}, você precisará de uma velocidade média de {speed}km por {(timeUnit == 1 ? "minuto" : "hora")}");
+
+// TODO: implementar lógica para mostrar tambem a velocidade de m/km
